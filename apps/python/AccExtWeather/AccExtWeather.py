66ec3ed024b42974fd5746ef4acf793c859062b8
leBluem <clarkemarky@gmail.com>
2019-01-24 18:52:07 +0100

    Add files via upload

--- apps/python/AccExtWeather/AccExtWeather.py
+++ apps/python/AccExtWeather/AccExtWeather.py
@@ -1,3 +1,5 @@
+# AccExtWeather.py - v0.9 - by x4fab, leBluem, Blamer
+
 try:
     import ac
     import acsys
@@ -5,19 +7,28 @@ try:
     import traceback
     import random
     import time
+    import re
     import configparser
 except ImportError:
     pass
 
 # option
-bIs4k = False
+gWrapDebugText = False
+gHideBG = False
+gHideWeather = False
+gAppsize = 1.0
+#sizeToggle = 0
 
 # vars
 app = 0
 label = 0
+labeldate = 0
 error = 0
 timer = 0
-sizeToggle = 0
+speed = 0
+day_offset = 0
+
+
 timeOffsetMinusSmallerButton = 0
 timeOffsetPlusSmallerButton = 0
 timeOffsetMinusSmallButton = 0
@@ -39,34 +50,37 @@ settingsFilePath = "apps\\python\\AccExtWeather\\settings\\settings.ini"
 documentSectionGeneral = "GENERAL"
 
 def appCreateMyself():
-    global app, label
+    global app, label, labeldate
     global timeOffsetMinusSmallButton, timeOffsetPlusSmallButton, timeOffsetMinusBigButton, timeOffsetPlusBigButton, timeOffsetMinusHugeButton, timeOffsetPlusHugeButton, timeOffsetMinusPlayButton, timeOffsetPlusPlayButton
     global timeOffsetMinusSmallerButton, timeOffsetPlusSmallerButton, timeOffsetMinusPlaySmallButton, timeOffsetPlusPlaySmallButton
     global timeOffsetMinusMonthButton, timeOffsetPlusMonthButton, timeOffsetMinusYearButton, timeOffsetPlusYearButton
-    global sizeToggle
-    global bIs4k
+    global sizeToggle, gAppsize, gHideBG
 
-    mult=1
     dx=50
     dy=15
     fntsize=12
-    if bIs4k==True:
-        dx=100
-        dy=30
-        mult=2
-        fntsize=16
+    mult=1.0
+    if gAppsize>=0.5 and gAppsize<=4.0:
+        mult=gAppsize
+        dx=dx*mult
+        dy=dy*mult
+        fntsize=fntsize*mult
 
     app = ac.newApp("Shaders Patch Weather")
     ac.setTitle(app, "   Weather FX")
     ac.setSize(app, 335*mult, 420*mult)
-    # ac.setVisible(app,1)
+
+    xpos=10
+    ypos=35
 
     label = ac.addLabel(app, "")
     ac.setFontSize(label, fntsize-1)
-    ac.setPosition(label, 10, dy*3+25)
+    ac.setPosition(label, 10, ypos+dy*3+3)
 
-    xpos=10
-    ypos=25
+    labeldate = ac.addLabel(app, "")
+    ac.setFontSize(labeldate, fntsize+1)
+    ac.setPosition(labeldate, 300*mult, ypos+dy*3+30)
+
     timeOffsetMinusSmallerButton = ac.addButton(app, "−5min")
     ac.setPosition(timeOffsetMinusSmallerButton, xpos, ypos)
     ac.setSize(timeOffsetMinusSmallerButton, 50*mult, dy-1)
@@ -103,9 +117,8 @@ def appCreateMyself():
     ac.setFontSize(timeOffsetPlusPlaySmallButton, fntsize)
     ac.addOnClickedListener(timeOffsetPlusPlaySmallButton, timeOffsetPlusPlaySmall)
 
-
     xpos=10
-    ypos=25+dy
+    ypos=35+dy
     timeOffsetMinusSmallButton = ac.addButton(app, "−45min")
     ac.setPosition(timeOffsetMinusSmallButton, xpos, ypos)
     ac.setSize(timeOffsetMinusSmallButton, 50*mult, dy-1)
@@ -142,9 +155,8 @@ def appCreateMyself():
     ac.setFontSize(timeOffsetPlusPlayButton, fntsize)
     ac.addOnClickedListener(timeOffsetPlusPlayButton, timeOffsetPlusPlay)
 
-
     xpos=10
-    ypos=25+dy*2
+    ypos=35+dy*2
     timeOffsetMinusBigButton = ac.addButton(app, "−6hr")
     ac.setPosition(timeOffsetMinusBigButton, xpos, ypos)
     ac.setSize(timeOffsetMinusBigButton, 50*mult, dy-1)
@@ -181,102 +193,77 @@ def appCreateMyself():
     ac.setFontSize(timeOffsetPlusPlayBigButton, fntsize)
     ac.addOnClickedListener(timeOffsetPlusPlayBigButton, timeOffsetPlusBigPlay)
 
-def getSettingsValue(parser, section, option, value, boolean = False):
-    if parser.has_option(str(section), str(option)):
-        if boolean:
-            return parser.getboolean(str(section), option)
-        else:
-            return parser.get(str(section), str(option))
-    else:
-        return setSettingsValue(parser, section, option, value)
-
-def appReadSettings():
-    global bIs4k, settingsFilePath, documentSectionGeneral
-    try:
-        if not os.path.isfile(settingsFilePath):
-            with open(settingsFilePath, "w") as sf:
-                ac.log("AccExtWeatherFX: Settings file created")
-        settingsParser = configparser.ConfigParser()
-        settingsParser.optionxform = str
-        settingsParser.read(settingsFilePath)
-        bIs4k = getSettingsValue(settingsParser, documentSectionGeneral, "uiDoubleSized", False, True)
-    except:
-        ac.log("Unexpected error:" + traceback.format_exc())
-
 def timeOffsetMinusSmaller(*args):
     try:
         ac.ext_weatherTimeOffset(-5 * 60)
     except:
-        ac.log("Unexpected error:" + traceback.format_exc())
+        ac.log("AccExtWeatherFX: Unexpected error:" + traceback.format_exc())
 
 def timeOffsetPlusSmaller(*args):
     try:
         ac.ext_weatherTimeOffset(5 * 60)
     except:
-        ac.log("Unexpected error:" + traceback.format_exc())
+        ac.log("AccExtWeatherFX: Unexpected error:" + traceback.format_exc())
 
 def timeOffsetMinusSmall(*args):
     try:
         ac.ext_weatherTimeOffset(-45 * 60)
     except:
-        ac.log("Unexpected error:" + traceback.format_exc())
+        ac.log("AccExtWeatherFX: Unexpected error:" + traceback.format_exc())
 
 def timeOffsetPlusSmall(*args):
     try:
         ac.ext_weatherTimeOffset(45 * 60)
     except:
-        ac.log("Unexpected error:" + traceback.format_exc())
+        ac.log("AccExtWeatherFX: Unexpected error:" + traceback.format_exc())
 
 def timeOffsetMinusBig(*args):
     try:
         ac.ext_weatherTimeOffset(-6 * 60 * 60)
     except:
-        ac.log("Unexpected error:" + traceback.format_exc())
+        ac.log("AccExtWeatherFX: Unexpected error:" + traceback.format_exc())
 
 def timeOffsetPlusBig(*args):
     try:
         ac.ext_weatherTimeOffset(6 * 60 * 60)
     except:
-        ac.log("Unexpected error:" + traceback.format_exc())
+        ac.log("AccExtWeatherFX: Unexpected error:" + traceback.format_exc())
 
 def timeOffsetMinusHuge(*args):
     try:
         ac.ext_weatherTimeOffset(-24 * 60 * 60)
     except:
-        ac.log("Unexpected error:" + traceback.format_exc())
+        ac.log("AccExtWeatherFX: Unexpected error:" + traceback.format_exc())
 
 def timeOffsetPlusHuge(*args):
     try:
         ac.ext_weatherTimeOffset(24 * 60 * 60)
     except:
-        ac.log("Unexpected error:" + traceback.format_exc())
+        ac.log("AccExtWeatherFX: Unexpected error:" + traceback.format_exc())
 
 def timeOffsetMinusMonth(*args):
     try:
         ac.ext_weatherTimeOffset(-30 * 24 * 60 * 60)
     except:
-        ac.log("Unexpected error:" + traceback.format_exc())
+        ac.log("AccExtWeatherFX: Unexpected error:" + traceback.format_exc())
 
 def timeOffsetPlusMonth(*args):
     try:
         ac.ext_weatherTimeOffset(30 * 24 * 60 * 60)
     except:
-        ac.log("Unexpected error:" + traceback.format_exc())
+        ac.log("AccExtWeatherFX: Unexpected error:" + traceback.format_exc())
 
 def timeOffsetMinusYear(*args):
     try:
         ac.ext_weatherTimeOffset(-365 * 24 * 60 * 60)
     except:
-        ac.log("Unexpected error:" + traceback.format_exc())
+        ac.log("AccExtWeatherFX: Unexpected error:" + traceback.format_exc())
 
 def timeOffsetPlusYear(*args):
     try:
         ac.ext_weatherTimeOffset(365 * 24 * 60 * 60)
     except:
-        ac.log("Unexpected error:" + traceback.format_exc())
-
-speed = 0
-day_offset = 0
+        ac.log("AccExtWeatherFX: Unexpected error:" + traceback.format_exc())
 
 def timeOffsetMinusPlaySmall(*args):
     global speed
@@ -302,8 +289,34 @@ def timeOffsetPlusBigPlay(*args):
     global speed
     speed += 50
 
+def getSettingsValue(parser, section, option, value, boolean = False):
+    if parser.has_option(str(section), str(option)):
+        if boolean:
+            return parser.getboolean(str(section), option)
+        else:
+            return parser.get(str(section), str(option))
+    else:
+        return setSettingsValue(parser, section, option, value)
+
+def appReadSettings():
+    global gWrapDebugText, gAppsize, gHideBG, gHideWeather
+    global settingsFilePath, documentSectionGeneral
+    try:
+        if not os.path.isfile(settingsFilePath):
+            with open(settingsFilePath, "w") as sf:
+                ac.log("AccExtWeatherFX: Settings file created")
+        settingsParser = configparser.ConfigParser()
+        settingsParser.optionxform = str
+        settingsParser.read(settingsFilePath)
+        gWrapDebugText = getSettingsValue(settingsParser, documentSectionGeneral, "uiWrapDebugText", False, True)
+        gHideBG = getSettingsValue(settingsParser, documentSectionGeneral, "uiHideBG", False, True)
+        gHideWeather = getSettingsValue(settingsParser, documentSectionGeneral, "uiHideWeather", False, True)
+        gAppsize = float(getSettingsValue(settingsParser, documentSectionGeneral, "uiSize", "1.0", False))
+    except:
+        ac.log("AccExtWeatherFX: Unexpected error:" + traceback.format_exc())
+
 def acUpdate(delta_t):
-    global error, timer, day_offset
+    global error, timer, day_offset, gWrapDebugText, gAppsize, gHideBG, gHideWeather
     timer += delta_t
     day_offset += speed * delta_t
     if timer > 0.05:
@@ -312,21 +325,37 @@ def acUpdate(delta_t):
             try:
                 ac.ext_weatherTimeOffset(24 * 60 * 60 * day_offset / abs(day_offset))
             except:
-                ac.log("Unexpected error:" + traceback.format_exc())
+                ac.log("AccExtWeatherFX: Unexpected error:" + traceback.format_exc())
             day_offset = 0
         try:
-            ac.setText(label, ac.ext_weatherDebugText())
+            s = ac.ext_weatherDebugText()
+            s = s.replace('current day:', ' > current day:                   ')
+            if gWrapDebugText==True:
+                s = '\n'.join(s.strip() for s in re.findall(r'.{1,80}(?:\s+|$)', s))
+                s = s.replace('>>> Sol weather: v', '\n>>> Sol weather: v')
+            if gHideWeather==True:
+                ss = s.split('\n')
+                s=ss[0] + '\n' + ss[2]
+            ac.setText(label, s)
+            if gHideBG==True:
+                try:
+                    ac.setBackgroundOpacity(app,0)
+                    ac.drawBorder(app,0)
+                except:
+                    ac.log("AccExtWeatherFX: Unexpected error:" + traceback.format_exc())
         except:
             if error < 10:
-                ac.log("Unexpected error:" + traceback.format_exc())
+                ac.log("AccExtWeatherFX: Unexpected error:" + traceback.format_exc())
             ac.setText(label, "Unexpected error:" + traceback.format_exc())
 
 def acMain(ac_version):
     try:
         appReadSettings()
         appCreateMyself()
+        # ac.setVisible(app,1) # todo debug out
     except:
-        ac.log("Unexpected error:" + traceback.format_exc())
+        ac.log("AccExtWeatherFX: Unexpected error:" + traceback.format_exc())
+    return "AccExtWeather"
 
 def acShutdown(*args):
-    ac.removeItem(app)
+    app.Close()
